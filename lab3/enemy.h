2538bf0503a3cab789152c9b1a148beba4162938
Kacper Lipiec <peter.lipiec@poczta.onet.pl>
2025-03-31 12:31:11 +0200

    Update enemy.h

--- lab3/enemy.h
+++ lab3/enemy.h
@@ -4,18 +4,6 @@
 #include <chrono>
 #include "object.h"
 
-/**
- * Proszę o utworzenie klasy `Enemy`, która będzie dziedziczyć po klasie `Object`
- * - Klasa ta powinna mieć mieć składową odpowiadającą ilości punktów życia, oraz getter: `lifePercent()` (**W PROCENTACH!**)
- * - Klasa powinna mieć konstruktor przyjmujący aktualną pozycje oraz maksymalne życie z wartością domyślną (dwu-argumentowy, który można wywołać z jednym argumentem)
- * - Klasa powinna mieć metodę `isAlive()` która zwraca prawdę jeśli wróg ma życie powyżej zera
- * - Klasa powinna mieć metodę `decreaseLife()` przyjmującą jako argument liczbę obrażeń
- * - Po zaimplementowaniu powyższego proszę zakomentować makro `UNIMPLEMENTED_enemy`
- */
-
-
-
-
 class Enemy : public Object {
 public:
     Enemy(const Position& pos, int maxHp = 100);
