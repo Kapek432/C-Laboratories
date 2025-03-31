09bfac6913c9b7be1092194d11b716614534c3a3
Kacper Lipiec <peter.lipiec@poczta.onet.pl>
2025-03-31 12:30:55 +0200

    Update bullet.h

--- lab3/bullet.h
+++ lab3/bullet.h
@@ -4,17 +4,6 @@
 #include "object.h"
 #include "direction.h"
 
-
-/**
- * Proszę o utworzenie klasy `Bullet`, która będzie dziedziczyć po klasie `Object`, następnie proszę zakomentować `UNIMPLEMENTED_bulletIsObject`
- * - Klasa ta powinna mieć mieć składową `direction_`, a także operujący na niej getter (`direction()`) i setter (`setDirection`)
- * - Klasa powinna mieć dwu-argumentowy konstruktor przyjmujący kierunek i pozycje początkową
- * - Po zaimplementowania tego proszę zakomentować `UNIMPLEMENTED_bulletHasDirectionAndPosition`
- * - Klasa powinna zawierać statyczny atrybut odpowiadający prędkości o wartości `2`, oraz getter `speed()`
- */
-
-
-
 class Bullet : public Object
 {
 Direction direction_;
